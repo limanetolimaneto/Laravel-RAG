c3345b42fccfe4a742880cb1de7eb3e023975203
Lima Neto <limaneto.sist@gmail.com>
2026-06-03 16:24:42 +0200

    Update from Vostro

--- database/migrations/2026_05_27_063558_create_embeddings_table.php
+++ database/migrations/2026_05_27_063558_create_embeddings_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -15,7 +16,15 @@ return new class extends Migration
             $table->id();
             $table->string('document_name');
             $table->longText('content');
-            $table->json('embedding');
+            // Embedding field based on Mysql version
+            // json <= '8.4.0' >= vector
+            $pdo = DB::connection()->getPdo();
+            $version = $pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
+            if (version_compare($version, '8.4.0', '>=')) {
+                $table->raw('embedding VECTOR(768) NOT NULL');
+            } else {
+                $table->json('embedding');
+            }
             $table->integer('chunk_index');
             $table->timestamps();
         });
